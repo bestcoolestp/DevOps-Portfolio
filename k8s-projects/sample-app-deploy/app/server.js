047ecb9438f4253809637ad68988cdec14dc63ae
bestcoolestp <108534975+bestcoolestp@users.noreply.github.com>
2026-01-04 11:32:27 +0900

    Add sample Node app and Dockerfile

---
+++ k8s-projects/sample-app-deploy/app/server.js
@@ -0,0 +1,19 @@
+const http = require("http");
+
+const port = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "application/json" });
+  res.end(
+    JSON.stringify({
+      status: "ok",
+      message: "Hello from Kubernetes!",
+      version: "1.0.0",
+      timestamp: new Date().toISOString(),
+    })
+  );
+});
+
+server.listen(port, () => {
+  console.log(`Node.js Server running on port ${port}`);
+});
